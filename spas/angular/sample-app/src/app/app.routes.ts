8d3977f2a2b3a5b3c2f8ab80911a1afdad6da836
Marvin Gajek <Marvin.Gajek1@web.de>
2024-02-01 21:36:05 +0100

    update inital sample Angular SPA setup

--- spas/angular/sample-app/src/app/app.routes.ts
+++ spas/angular/sample-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent},
+];

e00c8227a971abe1a5ba6bb542c9748d8b68c42a
Marvin Gajek <Marvin.Gajek1@web.de>
2024-02-02 20:56:30 +0100

    consider logout button element and it's logic for on click event

--- spas/angular/sample-app/src/app/components/home/home.component.ts
+++ spas/angular/sample-app/src/app/components/home/home.component.ts
@@ -1,18 +1,23 @@
 import { AuthService } from '@auth0/auth0-angular';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  constructor(private auth: AuthService) {}
+  constructor(public auth: AuthService) {}
 
   login() {
     this.auth.loginWithRedirect();
+  }
+
+  logout() {
+    this.auth.logout();
   }
   title: String = 'sample-app';
 }

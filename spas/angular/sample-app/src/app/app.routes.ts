9d29bf1a7766a7d30322142bd48ae5e087ce01e1
Marvin Gajek <Marvin.Gajek1@web.de>
2024-02-02 20:34:00 +0100

    utilize AuthGuard service

--- spas/angular/sample-app/src/app/app.routes.ts
+++ spas/angular/sample-app/src/app/app.routes.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { NgModule } from '@angular/core';
 import { AuthGuardedApiComponent } from './components/auth-guarded-api/auth-guarded-api.component';
+import { AuthGuard } from '@auth0/auth0-angular';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'auth-guarded-api', component: AuthGuardedApiComponent },
+  { path: 'auth-guarded-api', component: AuthGuardedApiComponent, canActivate: [AuthGuard], },
   { path: '**', component: PageNotFoundComponent},
 ];
 

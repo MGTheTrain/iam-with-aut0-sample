da0201fdf10f722ca864b64e5aa85efd149c28fc
Marvin Gajek <Marvin.Gajek1@web.de>
2024-02-02 21:05:57 +0100

    store JWT usable for HTTP requests

--- spas/angular/sample-app/src/app/components/home/home.component.ts
+++ spas/angular/sample-app/src/app/components/home/home.component.ts
@@ -10,7 +10,28 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  isAuthenticated: boolean = false;
+  accessToken: string | null = null; // Variable to store the ID token
+
   constructor(public auth: AuthService) {}
+
+  ngOnInit(): void {
+    this.auth.isAuthenticated$.subscribe((isAuthenticated) => {
+      this.isAuthenticated = isAuthenticated;
+
+      if (isAuthenticated) {
+        this.auth.getAccessTokenSilently().subscribe(
+          (accessToken: string) => {
+            this.accessToken = accessToken;
+            console.log(this.accessToken)
+          },
+          (error) => {
+            console.error('Error getting access token:', error);
+          }
+        );
+      }
+    });
+  }
 
   login() {
     this.auth.loginWithRedirect();

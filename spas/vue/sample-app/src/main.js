2548a59e265a9b6e727206ea3caf66d9642defc4
Marvin Gajek <Marvin.Gajek1@web.de>
2024-02-03 14:40:38 +0100

    move audience key to correct scope

--- spas/vue/sample-app/src/main.js
+++ spas/vue/sample-app/src/main.js
@@ -9,9 +9,9 @@ app.use(
   createAuth0({
     domain: 'dev-d81cx3ar5jc0hn2z.us.auth0.com', // Auth0 **application** specific value
     clientId: 'N7nDGcMjEwUSSn1RZjhSTPk59zUfWThK', // Auth0 **application** specific value
-    audience: 'https://quickstart/api', // Auth0 **API** specific value
     authorizationParams: {
-      redirect_uri: window.location.origin
+      redirect_uri: window.location.origin,
+      audience: 'https://quickstart/api', // Auth0 **API** specific value
     }
   })
 );

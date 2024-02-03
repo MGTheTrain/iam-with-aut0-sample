63bcfcadeca6e6d742ac056110b672ae4e5034e5
Marvin Gajek <Marvin.Gajek1@web.de>
2024-02-03 14:01:40 +0100

    update vue component and consider auth0-vue API in main.js

--- spas/vue/sample-app/src/main.js
+++ spas/vue/sample-app/src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createAuth0 } from '@auth0/auth0-vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(
+  createAuth0({
+    domain: 'dev-d81cx3ar5jc0hn2z.us.auth0.com', // Auth0 **application** specific value
+    clientId: 'N7nDGcMjEwUSSn1RZjhSTPk59zUfWThK', // Auth0 **application** specific value
+    audience: 'https://quickstart/api', // Auth0 **API** specific value
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    }
+  })
+);
+
+app.mount('#app');

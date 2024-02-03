cd71b0e0be36c252eafc8f3f98341ceca021ce71
Marvin Gajek <Marvin.Gajek1@web.de>
2024-02-03 14:27:59 +0100

    update logic in home component

--- spas/vue/sample-app/src/components/HelloWorld.vue
+++ spas/vue/sample-app/src/components/HelloWorld.vue
@@ -3,8 +3,8 @@
     <h1 class="display-3">{{ title }}</h1>
     <p class="lead">Welcome to {{ title }}!</p>
     <div class="d-flex justify-content-center align-items-center">
-      <button v-if="!this.$auth0.isAuthenticated" @click="login" class="btn btn-primary">Login</button>
-      <button v-if="this.$auth0.isAuthenticated" @click="logout" class="btn btn-success">Logout</button>
+      <button v-if="!isAuthenticated" @click="login" class="btn btn-primary">Login</button>
+      <button v-if="isAuthenticated" @click="logout" class="btn btn-success">Logout</button>
     </div>
   </div>
 </template>
@@ -20,24 +20,35 @@
 </style>
 
 <script>
+import { ref, onUpdated } from 'vue';
+import { useAuth0 } from '@auth0/auth0-vue';
 
 export default {
-  data() {
+  setup() {
+    const title = 'sample-app';
+    const { isAuthenticated, loginWithRedirect, logout, getAccessTokenSilently } = useAuth0();
+    const accessToken = ref(null);
+
+    onUpdated(async () => {
+      // console.log(isAuthenticated.value); // lifecycle hook should be onUpdated and not onMounted. See: https://vuejs.org/guide/essentials/lifecycle
+      if (isAuthenticated.value) {
+        try {
+          const token = await getAccessTokenSilently();
+          accessToken.value = token;
+          console.log('Access Token:', accessToken.value);
+        } catch (error) {
+          console.error('Error getting access token:', error);
+        }
+      }
+    });
+
     return {
-      title: "sample-app",
-      auth: {
-        isAuthenticated: false, 
-      },
+      title,
+      isAuthenticated,
+      login: loginWithRedirect,
+      logout,
+      accessToken,
     };
   },
-  
-  methods: {
-    login() {
-      this.$auth0.loginWithRedirect();
-    },
-    logout() {
-      this.$auth0.logout();
-    }
-  }
 };
 </script>

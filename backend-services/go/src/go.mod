f3a31fd6531d42a4dff4784ff3b1184e695d3919
Marvin Gajek <Marvin.Gajek1@web.de>
2024-02-04 14:01:07 +0100

    adjust module pathes

--- backend-services/go/src/go.mod
+++ backend-services/go/src/go.mod
@@ -6,9 +6,6 @@ require (
 	github.com/auth0/go-jwt-middleware/v2 v2.2.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/gwatts/gin-adapter v1.0.0
-	github.com/joho/godotenv v1.5.1
-	github.com/swaggo/files v1.0.1
-	github.com/swaggo/gin-swagger v1.6.0
 	github.com/swaggo/swag v1.16.3
 )
 
--- backend-services/go/src/main.go
+++ backend-services/go/src/main.go
@@ -1,48 +1,48 @@
-package main
+// package main
 
-import (
-	"log"
+// import (
+// 	"log"
 
-	"github.com/MGTheTrain/iam-with-auth0-sample/backend-services/go/src/controllers"
-	"github.com/MGTheTrain/iam-with-auth0-sample/backend-services/go/src/docs" // IMPORTANT: Folder with swagger.json, etc. need to be generated via `swag init` -> consider controller which implement endpoint logic, e.g. ./controllers/http_controller.go
-	"github.com/MGTheTrain/iam-with-auth0-sample/backend-services/go/src/middleware"
+// 	"github.com/MGTheTrain/iam-with-auth0-sample/backend-services/go/src/controllers"
+// 	"github.com/MGTheTrain/iam-with-auth0-sample/backend-services/go/src/docs" // IMPORTANT: Folder with swagger.json, etc. need to be generated via `swag init` -> consider controller which implement endpoint logic, e.g. ./controllers/http_controller.go
+// 	"github.com/MGTheTrain/iam-with-auth0-sample/backend-services/go/src/middleware"
 
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
-)
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/joho/godotenv"
+// 	swaggerFiles "github.com/swaggo/files"
+// 	ginSwagger "github.com/swaggo/gin-swagger"
+// )
 
-// @title Simple Service API
-// @version 1.0
-// @description Simple Service API sample
-// @termsOfService http://localhost:3010/swagger/doc.json
-// @host localhost:3010
-// @BasePath /api/v1/sas
-// @schemes http
-// @securityDefinitions.apiKey ApiKeyAuth
-// @in header
-// @name Authorization
-func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading the .env file: %v", err)
-	}
+// // @title Simple Service API
+// // @version 1.0
+// // @description Simple Service API sample
+// // @termsOfService http://localhost:3010/swagger/doc.json
+// // @host localhost:3010
+// // @BasePath /api/v1/sas
+// // @schemes http
+// // @securityDefinitions.apiKey ApiKeyAuth
+// // @in header
+// // @name Authorization
+// func main() {
+// 	if err := godotenv.Load(); err != nil {
+// 		log.Fatalf("Error loading the .env file: %v", err)
+// 	}
 
-	router := gin.Default()
+// 	router := gin.Default()
 
-	// Swagger route
-	docs.SwaggerInfo.Version = "v1"
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	controllers.RegisterPublicApiEndpoints(router)
+// 	// Swagger route
+// 	docs.SwaggerInfo.Version = "v1"
+// 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+// 	controllers.RegisterPublicApiEndpoints(router)
 
-	router.Use(middleware.EnsureValidToken())
-	controllers.RegisterPrivateApiEndpoints(router)
+// 	router.Use(middleware.EnsureValidToken())
+// 	controllers.RegisterPrivateApiEndpoints(router)
 
-	router.Use(middleware.CheckPermissions("admin:permission"))
-	controllers.RegisterPrivateApiEndpointsWithRBAC(router)
+// 	router.Use(middleware.CheckPermissions("admin:permission"))
+// 	controllers.RegisterPrivateApiEndpointsWithRBAC(router)
 
-	log.Print("Server listening on http://localhost:3010")
-	if err := router.Run(":3010"); err != nil {
-		log.Fatalf("There was an error with the http server: %v", err)
-	}
-}
+// 	log.Print("Server listening on http://localhost:3010")
+// 	if err := router.Run(":3010"); err != nil {
+// 		log.Fatalf("There was an error with the http server: %v", err)
+// 	}
+// }

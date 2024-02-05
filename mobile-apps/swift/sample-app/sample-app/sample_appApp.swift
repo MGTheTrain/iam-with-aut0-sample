6b36ab60c48f3f1b48a064298f94f03e92d93c58
Marvin Gajek <marvin.gajek@outlook.com>
2024-02-05 23:02:20 +0100

    setup initial swift project

---
+++ mobile-apps/swift/sample-app/sample-app/sample_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_appApp.swift
+//  sample-app
+//
+//  Created by Marvin Gajek on 05.02.24.
+//
+
+import SwiftUI
+
+@main
+struct sample_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
